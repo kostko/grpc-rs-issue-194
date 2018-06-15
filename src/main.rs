2fa5af92a3ee697a741313b4144b0e33602fc39e
Jernej Kos <jernej@kos.mx>
2018-06-15 16:38:48 +0200

    Add version based on futures-cpupool executor

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
+#![allow(deprecated)]
+
 extern crate grpcio;
 extern crate futures;
+#[cfg(feature = "cpupool")]
+extern crate futures_cpupool;
 
+#[cfg(not(feature = "cpupool"))]
 use std::sync::Arc;
 
 use futures::sync::{mpsc, oneshot};
 use futures::{future, Future, Stream};
 
 fn main() {
-    let environment = Arc::new(grpcio::Environment::new(4));
-
     // Create an executor based on grpcio. This hack is required because the
     // spawn functionality is not otherwise exposed.
-    let executor = grpcio::Client::new(grpcio::ChannelBuilder::new(environment).connect(""));
+    #[cfg(not(feature = "cpupool"))]
+    let executor = {
+        let environment = Arc::new(grpcio::Environment::new(4));
+        grpcio::Client::new(grpcio::ChannelBuilder::new(environment).connect(""))
+    };
+    #[cfg(feature = "cpupool")]
+    let executor = futures_cpupool::CpuPool::new(4);
 
     let (ch1_tx, ch1_rx) = mpsc::unbounded::<oneshot::Sender<()>>();
     let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
@@ -28,7 +37,7 @@ fn main() {
 
     let joined = future::join_all(vec![future1, future2]);
 
-    executor.spawn(joined.map(|_| ()));
+    drop(executor.spawn(joined.map(|_| ())));
 
-    shutdown_rx.wait().unwrap();
+    drop(shutdown_rx.wait());
 }
